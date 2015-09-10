555eac3111391dc4650915d134fd181752be72e5
Nexxado <ndraiman@gmail.com>
2015-09-10 13:51:21 +0300

    added some SQL functionality - not completed.

--- app/src/main/java/com/gmail/ndraiman/wifipasswords/MainActivity.java
+++ app/src/main/java/com/gmail/ndraiman/wifipasswords/MainActivity.java
@@ -1,18 +1,29 @@
 package com.gmail.ndraiman.wifipasswords;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private RecyclerView listWifi;
     private WifiListAdapter listAdapter;
+
+    private boolean isDbExists = false;
+    SQLiteDatabase passwordsDB = null;
+    private final String DB_NAME = "WifiPasswords";
+    private final String TABLE_NAME = "passwords";
 
 
     @Override
@@ -36,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         //TODO Refresh database from wpa_supplicant.conf
 
+        openOrCreateDatabase();
 
     }
 
@@ -61,6 +73,63 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onDestroy() {
+        passwordsDB.close();
+        super.onDestroy();
+    }
+
+    /******************
+     * SQLite Methods
+     ******************/
+    public void openOrCreateDatabase() {
+        try {
+            passwordsDB = this.openOrCreateDatabase(DB_NAME, MODE_PRIVATE, null);
+            if(isDbExists) {
+                if(passwordsDB.isOpen()) {
+                    Toast.makeText(this, "Database Opened", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            passwordsDB.execSQL("CREATE TABLE IF NOT EXISTS "+ TABLE_NAME
+                    + " (id integer primary key, name VARCHAR, pass VARCHAR);");
+
+
+            //File database = getApplicationContext().getDatabasePath("WifiPasswords.db");
+            if (passwordsDB.isOpen()) { //database.exists()
+                Toast.makeText(this, "Database created successfully", Toast.LENGTH_SHORT).show();
+                isDbExists = true;
+            } else {
+                Toast.makeText(this, "Error creating Database", Toast.LENGTH_SHORT).show();
+                isDbExists = false;
+            }
+
+        } catch (Exception e) {
+            Log.e("SQLite DB", "SQLite createDatabase() exception" + e);
+            e.printStackTrace();
+        }
+    }
+
+    public void addEntry(String title, String password) {
+
+        passwordsDB.execSQL("INSERT INTO " + TABLE_NAME + " (name, pass) VALUES ('" +
+                title + "', '" + password + "');");
+    }
+
+    public void getEntries() {
+        // A Cursor provides read and write access to database results
+        Cursor cursor = passwordsDB.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+
+        // Get the index for the column name provided
+        int idColumn = cursor.getColumnIndex("id");
+        int nameColumn = cursor.getColumnIndex("name");
+        int passColumn = cursor.getColumnIndex("pass");
+
+        cursor.moveToFirst();
+
+        //TODO Add each entry to a RecyclerView row
+    }
 
 
 }
