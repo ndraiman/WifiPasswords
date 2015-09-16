678b1d036a744e409e001bdf8db8391af8a9161e
Nexxado <ndraiman@gmail.com>
2015-09-17 00:07:52 +0300

    simplified animateSunblind boolean checks

--- app/src/main/java/com/gmail/ndraiman/wifipasswords/recycler/AnimationUtils.java
+++ app/src/main/java/com/gmail/ndraiman/wifipasswords/recycler/AnimationUtils.java
@@ -10,19 +10,17 @@ import android.view.animation.BounceInterpolator;
 import android.view.animation.CycleInterpolator;
 import android.view.animation.DecelerateInterpolator;
 
-/**
- * Created by ND88 on 16/09/2015.
- */
+
 public class AnimationUtils {
 
 
     public static void animateSunblind(RecyclerView.ViewHolder holder, boolean goesDown) {
         int holderHeight = holder.itemView.getHeight();
-        holder.itemView.setPivotY(goesDown == true ? 0 : holderHeight);
+        holder.itemView.setPivotY(goesDown ? 0 : holderHeight);
         holder.itemView.setPivotX(holder.itemView.getHeight());
         AnimatorSet animatorSet = new AnimatorSet();
-        ObjectAnimator animatorTranslateY = ObjectAnimator.ofFloat(holder.itemView, "translationY", goesDown == true ? 300 : -300, 0);
-        ObjectAnimator animatorRotation = ObjectAnimator.ofFloat(holder.itemView, "rotationX", goesDown == true ? -90f : 90, 0f);
+        ObjectAnimator animatorTranslateY = ObjectAnimator.ofFloat(holder.itemView, "translationY", goesDown ? 300 : -300, 0);
+        ObjectAnimator animatorRotation = ObjectAnimator.ofFloat(holder.itemView, "rotationX", goesDown ? -90f : 90, 0f);
         ObjectAnimator animatorScaleX = ObjectAnimator.ofFloat(holder.itemView, "scaleX", 0.5f, 1f);
         animatorSet.playTogether(animatorTranslateY, animatorRotation, animatorScaleX);
         animatorSet.setInterpolator(new DecelerateInterpolator(1.1f));
