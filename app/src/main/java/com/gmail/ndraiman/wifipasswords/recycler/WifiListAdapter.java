c7bed4c191de42331bae0e196356be0f43dcaa3c
Netanel Draiman <ndraiman@gmail.com>
2015-09-16 16:49:08 +0300

    removed unused gradle dependencies
    issues: RecyclerView has no ripple effect

--- app/src/main/java/com/gmail/ndraiman/wifipasswords/recycler/WifiListAdapter.java
+++ app/src/main/java/com/gmail/ndraiman/wifipasswords/recycler/WifiListAdapter.java
@@ -1,8 +1,6 @@
 package com.gmail.ndraiman.wifipasswords.recycler;
 
 import android.content.Context;
-import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,8 +12,6 @@ import com.gmail.ndraiman.wifipasswords.pojo.WifiEntry;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import jp.wasabeef.recyclerview.animators.holder.AnimateViewHolder;
 
 
 public class WifiListAdapter extends RecyclerView.Adapter<WifiListAdapter.MyViewHolder> {
@@ -88,47 +84,47 @@ public class WifiListAdapter extends RecyclerView.Adapter<WifiListAdapter.MyView
     }
 
     /*****************************************************************************/
-    //Testing Custom Animation ViewHolder
+    //Testing Custom Animation ViewHolder - add "compile 'jp.wasabeef:recyclerview-animators:1.2.0@aar' " to gradle
     /*****************************************************************************/
-    class MyCustomViewHolder extends AnimateViewHolder {
-
-        private TextView wifiTitle;
-        private TextView wifiPassword;
-
-        public MyCustomViewHolder(View itemView) {
-            super(itemView);
-
-            wifiTitle = (TextView) itemView.findViewById(R.id.title_wifi);
-            wifiPassword = (TextView) itemView.findViewById(R.id.password_wifi);
-
-        }
-
-        @Override
-        public void animateAddImpl(ViewPropertyAnimatorListener listener) {
-            ViewCompat.animate(itemView)
-                    .translationY(0)
-                    .alpha(1)
-                    .setDuration(300)
-                    .setListener(listener)
-                    .start();
-        }
-
-        @Override
-        public void animateRemoveImpl(ViewPropertyAnimatorListener listener) {
-            ViewCompat.animate(itemView)
-                    .translationY(-itemView.getHeight() * 0.3f)
-                    .alpha(0)
-                    .setDuration(300)
-                    .setListener(listener)
-                    .start();
-        }
-
-        @Override
-        public void preAnimateAddImpl() {
-            ViewCompat.setTranslationY(itemView, -itemView.getHeight() * 0.3f);
-            ViewCompat.setAlpha(itemView, 0);
-        }
-    }
+//    class MyCustomViewHolder extends AnimateViewHolder {
+//
+//        private TextView wifiTitle;
+//        private TextView wifiPassword;
+//
+//        public MyCustomViewHolder(View itemView) {
+//            super(itemView);
+//
+//            wifiTitle = (TextView) itemView.findViewById(R.id.title_wifi);
+//            wifiPassword = (TextView) itemView.findViewById(R.id.password_wifi);
+//
+//        }
+//
+//        @Override
+//        public void animateAddImpl(ViewPropertyAnimatorListener listener) {
+//            ViewCompat.animate(itemView)
+//                    .translationY(0)
+//                    .alpha(1)
+//                    .setDuration(300)
+//                    .setListener(listener)
+//                    .start();
+//        }
+//
+//        @Override
+//        public void animateRemoveImpl(ViewPropertyAnimatorListener listener) {
+//            ViewCompat.animate(itemView)
+//                    .translationY(-itemView.getHeight() * 0.3f)
+//                    .alpha(0)
+//                    .setDuration(300)
+//                    .setListener(listener)
+//                    .start();
+//        }
+//
+//        @Override
+//        public void preAnimateAddImpl() {
+//            ViewCompat.setTranslationY(itemView, -itemView.getHeight() * 0.3f);
+//            ViewCompat.setAlpha(itemView, 0);
+//        }
+//    }
     /*****************************************************************************/
     /*****************************************************************************/
 
